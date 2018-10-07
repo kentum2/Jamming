4f6917dba1ba497c75ab47fd1b9acc2a3be52e90
kentum2 <kentum2@gmail.com>
2018-10-07 16:49:22 +0300

    Add files via upload
    jamming additional update

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,51 +2,49 @@ import React from 'react';
 import './track.css';
 
 class Track extends React.Component {
-  constructor(props) {
-    super(props);
-  
-    this.addTrack=this.addTrack.bind(this);
-    this.removeTrack=this.removeTrack.bind(this);
-  }
-    addTrack() {
-      this.props.onAdd(this.props.track);
-      
-    }
-    removeTrack() {
-      this.props.onRemove(this.props.track);
+    constructor(props) {
+        super(props)
+
+        this.addTrack = this.addTrack.bind(this);
+        this.removeTrack = this.removeTrack.bind(this);
     }
 
     renderAction() {
-      if (this.props.isRemoval) {
-    return (
-      <a id={this.props.track.key}
-         className="Track-action"
-         onClick={this.removeTrack}>-</a>
-        )
-      } else {
-        return (
-          <a id={this.props.track.key}
-          className="Track-action"
-          onClick={this.addTrack}>+</a>
-    )
-  }
-  
-}
- 
-    render() {
-      return (
-        <div className="Track">
-            <div className="Track-information">
-              <h3>{this.props.track.name}</h3>
-              <p>{this.props.track.artist} | {this.props.track.album}</p>
-            </div>
-             {this.renderAction()}
-        </div>
-      );
+        if (this.props.isRemoval) {
+            return <a className="Track-action" onClick={this.removeTrack}>-</a>;
+        } else {
+            return <a className="Track-action" onClick={this.addTrack}>+</a>;
+        }
     }
-  }
 
-  export default Track;
+    addTrack() {
+        this.props.onAdd(this.props.track)
+    }
 
+    removeTrack() {
+        this.props.onRemove(this.props.track)
+    }
+    
+    render() {
+        return ( 
+            <div className="Track">
+                <div className="Track-information">
+                    <div className="Track-box">
+                        <div>
+                            <h4>{this.props.track.name}</h4>
+                            <p>{this.props.track.artist} | {this.props.track.album}</p>
+                        </div>
+                        <audio id="player" controls>
+                            <source src={this.props.track.preview_url} type="audio/mpeg" />
+                        </audio>
+                    </div>
+                </div>
+                { this.props.isRemoval ? <a className="Track-action" onClick={this.removeTrack} >-</a> : <a className="Track-action" onClick={this.addTrack}>+</a> }
+            </div>
+        )
+    }
+}
+
+export default Track;
 
   
